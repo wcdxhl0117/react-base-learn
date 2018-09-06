73089d301fcd13a7c688e0387efb8fb3dc960c21
chao.wang <chao.wang@elanking.com>
2018-09-06 17:54:17 +0800

    增加组件文件夹

---
+++ src/wechat/compentents/rule.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './rule.css'
+
+class App extends Component {
+	constructor(props) {
+	  super(props);
+		this.state = {
+			
+		}
+	}
+  render() {
+    return (
+      <div className="rule">
+        这里是规则
+      </div>
+    );
+  }
+
+  showRule () {
+    
+  }
+}
+
+export default App;
--- src/wechat/share.js
+++ src/wechat/share.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import './share.css'
+import Rule from './compentents/rule'
 
 class App extends Component {
 	constructor(props) {
 	  super(props);
 		this.state = {
-			
+			ruleShow: 'display: none'
 		}
 	}
   render() {
     return (
       <div className="container">
         <div className="wrap">
-        <div className="topImg">
-          <img src={require("./img/itrTop1.png")} />
-          <div>活动规则</div>
+          <div className="topImg">
+            <img src={require("./img/itrTop1.png")} />
+            <div onClick={this.showRule.bind(this)}>活动规则</div>
+          </div>
         </div>
-        </div>
+        {/* 规则 */}
+        <Rule />
       </div>
     );
+  }
+
+  showRule () {
+    
   }
 }
 
