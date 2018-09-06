c47dda6b6d2b7355ff4d463442c84fe087c0662c
chao.wang <wcd126yx@126.com>
2018-09-06 14:03:08 +0800

    新建首页

--- src/wechat/share.js
+++ src/wechat/share.js
@@ -4,34 +4,18 @@ class App extends Component {
 	constructor(props) {
 	  super(props);
 		this.state = {
-			inputVulue: '我是最开始的值',
-			list: ['麻痹我是第一项', '大爷的我是第二项']
+			
 		}
 	}
   render() {
     return (
-      <div className="App" onClick={this.clickEvent.bind(this)}>
-				{this.state.inputVulue}
-				{
-					// 列表 
-				}
-				<ul>
-					{
-						this.state.list.map((item, index) => {
-							return <li key={index}>{item}</li>
-						})
-					}
-				</ul>
+      <div className="App">
+				123
       </div>
     );
   }
 	
-	clickEvent() {
-		// 改变 
-		this.setState({
-			inputVulue : '我是改变的值'
-		})
-	}
+	
 }
 
 export default App;
