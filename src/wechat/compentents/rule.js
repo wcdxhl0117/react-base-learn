3a3ea7f491520c3397deed4d530bdb4532056a87
chao.wang <wcd126yx@126.com>
2018-09-08 16:56:33 +0800

    父子组建通讯，方法传递

--- src/wechat/compentents/rule.js
+++ src/wechat/compentents/rule.js
@@ -8,17 +8,22 @@ class App extends Component {
 
 		}
 	}
+
   render() {
     return (
       <div className={ `rule ${this.props.show?'showRule':''}` }>
-        {  }
+        <div className='popContent'>
+					<div className="paragraph">1.活动时间：即日起至2018.09.07;</div>
+					<div className="paragraph">2.每个用户可领取1次会员，每天最多给别人助力3次;</div>
+					<div className="paragraph">3.邀请7位好友助力后，才可获得7天VIP培优会员;</div>
+					<div className="paragraph">4.新关注公众号的用户可额外再获得1天VIP培优会员（每个微信号仅可得1次);</div>
+					<div className="paragraph">5.获得的VIP培优会员预计在24小时内到账，如有问题，请添加客服微信：yoomath1 进行咨询;</div>
+					<div onClick={this.props.hideRule} className="btn">知道了</div>
+			</div>
       </div>
     );
   }
 
-  showRule () {
-
-  }
 }
 
 export default App;
--- src/wechat/share.js
+++ src/wechat/share.js
@@ -6,7 +6,7 @@ class App extends Component {
 	constructor(props) {
 	  super(props);
 		this.state = {
-			ruleShow: true
+			ruleShow: false
 		}
 	}
   render() {
@@ -16,23 +16,31 @@ class App extends Component {
 					{/* top--img */}
           <div className="topImg">
             <img src={require("./img/itrTop1.png")} />
-            <div className="rulePop" onClick={this.showRule.bind(this)}>活动规则</div>
+            <div
+							className="rulePop"
+							onClick={this.showRule.bind(this)}
+						>活动规则</div>
           </div>
         </div>
 
 				{/* btm 结构 */}
 				<div id="content">
-
+					<div className="topImg">
+						<img src={require("./img/1.png")} />
+					</div>
+					<img className="btmImg" src={require("./img/2.jpg")} />
 				</div>
 
         {/* 规则 */}
-        <Rule show={this.state.ruleShow} />
+        <Rule hideRule={this.showRule.bind(this)} show={this.state.ruleShow} />
       </div>
     );
   }
 
   showRule () {
-
+		this.setState({
+      ruleShow: !this.state.ruleShow
+    });
   }
 }
 
