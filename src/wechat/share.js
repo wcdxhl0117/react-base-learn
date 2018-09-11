36bdd39c16214bf37821460068ed54e482464964
chao.wang <wcd126yx@126.com>
2018-09-11 14:01:56 +0800

    拖动

--- src/wechat/share.js
+++ src/wechat/share.js
@@ -7,7 +7,13 @@ class App extends Component {
 	constructor(props) {
 	  super(props);
 		this.state = {
-			ruleShow: false
+			ruleShow: false,
+			startY: '',
+			moveY: '',
+			moveLong: '',
+			initH: '',
+			minH: '',
+			maxH: ''
 		}
 	}
   render() {
@@ -16,7 +22,7 @@ class App extends Component {
         <div className="wrap">
 					{/* top--img */}
           <div className="topImg">
-            <img src={require("./img/itrTop1.png")} />
+            <img src={require("./img/itrTop1.png")} alt='' />
             <div
 							className="rulePop"
 							onClick={this.showRule.bind(this)}
@@ -27,17 +33,24 @@ class App extends Component {
 				{/* btm 结构 */}
 				<div id="content">
 					<div className="topImg">
-						<img src={require("./img/1.png")} />
+						<img src={require("./img/1.png")} alt='' />
 					</div>
-					<img onClick={this.goDetail.bind(this)} className="btmImg" src={require("./img/2.jpg")} />
+					<img onClick={this.goDetail.bind(this)} className="btmImg" src={require("./img/2.jpg")} alt='' />
 				</div>
 				{/* 上拉组建 */}
-		    <div className="btmWrap">
+		    <div className="btmWrap"
+					ref={this.btmWrap}
+					onTouchStart={this.Tstart.bind(this)}
+					onTouchMove={this.Tmove.bind(this)}
+					onTouchEnd={this.Tend.bind(this)}
+				>
 		        <div className="topTitle">此活动必须父母才能领取</div>
 		        <div id="toParent" className="comBtn">微信分享给父母，进行解锁领取</div>
 		        <div id="toQQ" className="comBtn">QQ分享给父母，进行解锁领取</div>
 		        <div id="toStu" className="comBtn">将好消息告诉其他同学</div>
 		    </div>
+				<div ref="minH" className="minH"></div>
+		    <div ref="maxH" className="maxH"></div>
         {/* 规则 */}
         <Rule hideRule={this.showRule.bind(this)} show={this.state.ruleShow} />
       </div>
@@ -60,6 +73,47 @@ class App extends Component {
       .catch(function (error) {
           console.log(error);
       })
+	}
+
+	// onTouchstart
+	Tstart (e) {
+		const touch = e.touches[0]
+		this.setState({
+			startY: touch.pageY,
+			initH: document.querySelectorAll('.btmWrap')[0].clientHeight,
+			minH: document.querySelectorAll('.minH')[0].clientHeight,
+			maxH: document.querySelectorAll('.maxH')[0].clientHeight
+		})
+	}
+	// onTouchMove
+	Tmove (e) {
+		const touch = e.touches[0]
+		this.setState({
+			moveY: touch.pageY,
+			{/* 笔记本键盘真是特别难用，特别是在使用数字真是别不爽*/}
+			moveLong: this.state.startY - this.state.moveY
+		})
+		let AllH = this.state.startY - this.state.moveY + this.state.initH
+		if (AllH > this.state.maxH) {
+				AllH = this.state.maxH
+		} else if (AllH < this.state.minH) {
+				AllH = this.state.minH
+		} else {
+				AllH = this.state.startY - this.state.moveY + this.state.initH
+		}
+		document.querySelectorAll('.btmWrap')[0].style.height = AllH + 'px'
+	}
+	// onTouchEnd
+	Tend () {
+		if (this.state.moveLong > 30) {
+				document.querySelectorAll('.btmWrap')[0].style.height = this.state.maxH + 'px'
+		} else if (this.state.moveLong > 0 && this.state.moveLong <= 30) {
+				document.querySelectorAll('.btmWrap')[0].style.height = this.state.minH + 'px'
+		} else if (this.state.moveLong < -30) {
+				document.querySelectorAll('.btmWrap')[0].style.height = this.state.minH + 'px'
+		} else if (this.state.moveLong > -30 && this.state.moveLong < 0) {
+				document.querySelectorAll('.btmWrap')[0].style.height = this.state.maxH + 'px'
+		}
 	}
 }
 
