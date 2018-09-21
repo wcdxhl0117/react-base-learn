b3c6b43fda95016a5f239cbfa5f08d29956680cb
chao.wang <chao.wang@elanking.com>
2018-09-21 16:36:56 +0800

    棋盘游戏快要成功的节奏啊

--- src/doc-learn/index.js
+++ src/doc-learn/index.js
@@ -6,24 +6,27 @@ import {
 
 {/* 方格组件 */}
 class Square extends React.Component {
-	constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick({value: 'X'})}>
+        {this.props.value}
       </button>
     );
   }
 }
 {/* 棋盘组件 */}
 class Board extends React.Component {
+	constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+			value={this.state.squares[i]}
+			onClick={() => this.handleClick(i)}
+		/>;
   }
   render() {
     const status = 'Next player: X';
