159340ef076ca244623be9ceb1f84979f6d2a2d1
chao.wang <wcd126yx@126.com>
2018-10-05 10:55:11 +0800

    北方你

--- src/App.js
+++ src/App.js
@@ -14,7 +14,5 @@ class App extends Component {
       </div>
     );
   }
-
-}
-
+}\
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+  constructor() {
+
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
