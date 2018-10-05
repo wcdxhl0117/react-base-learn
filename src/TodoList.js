91267012c444838141c8c8dc39346a52e33d6cfd
wangchaoxie <wcd126yx@126.com>
2018-10-05 12:52:56 +0800

    todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class TodoList extends Component {
-  constructor() {
-
-  }
   render() {
     return (
       <div>
-        <div></div>
+        ti
       </div>
     )
   }
 }
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoList from './TodoList';
 // PWA, 在支持https服务器上，具备：如果没网，也能看到网页缓存
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
