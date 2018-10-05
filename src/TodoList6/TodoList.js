7cec6c156df9934b460907b3428db7789f720005
wangchaoxie <wcd126yx@126.com>
2018-10-05 21:58:49 +0800

    虚拟dom分析

--- src/TodoList6/TodoList.js
+++ src/TodoList6/TodoList.js
@@ -2,6 +2,43 @@ import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem'
 import './style.css'
 
+// 虚拟Dom理论:如果没有react,自己实现render的思路
+/*
+	方法一
+		1.定义state数据
+		2.有一个模板,对应react中render的jsx
+		3.把数据和模板相结合,生成真实DOM,渲染到页面
+		4.state改变
+		5.数据加模板生成真是DOM,替换原始DOM
+		致命缺陷:第一次生成一个完整的DOM片段,第二次又称成一个完整的DOM片段,第二次DOM替换第一次的DOM,非常耗性能
+	
+	方法二:
+		1.定义state数据
+		2.有一个模板,对应react中render的jsx
+		3.把数据和模板相结合,生成真实DOM,渲染到页面
+		4.state改变
+		5.数据加模板结合,生成真实DOM,但并不替换原始DOM
+		6.新的DOM和原始DOM做对比,找差异
+		7.找出发生的变化
+		8.只用新的DOM中改变的部分替换老的DOM中对应的DOM
+		这种方式性能有所提升,因为DOM比对很耗新能,替换部分节约很多性能
+		问题:虽然提升了DOM替换的性能,但有消耗了对比的性能,性能提升不明显
+
+	方法三(react的方式):
+		1.定义state数据
+		2.有一个模板,对应react中render的jsx
+		3.把数据和模板相结合,生成真实DOM,来显示
+			真实:<div id='abc'><span>hello word</span></div>
+			虚拟:['div',{id: 'abc'}, ['span, {}, 'hello world']]
+		4.生成虚拟DOM(虚拟DOM就是一个js对象,用来描述真是DOM)
+		5.state发生变化
+		6.生成新的虚拟DOM   ['div',{id: 'abc'}, ['span, {}, 'ha ha']]
+		7.比较原始虚拟dom和新的虚拟dom的区别,找到区别是span内容
+		8.直接操作DOM,改变span中的内容
+		优点:js创建一个js对象很简单,但是创建一个dom的性能损耗是比较大的
+*/
+
+
 class TodoList extends Component {
 	constructor(props) {
 		super(props)
