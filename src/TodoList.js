1353cd8591ff1b206598839a5d9ba5f26c69c89d
wangchaoxie <wcd126yx@126.com>
2018-10-05 13:24:54 +0800

    react数据定义

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,11 +2,21 @@ import React, { Component, Fragment } from 'react';
 // 引入一个不被显示的包裹元素---Fragment占位符
 
 class TodoList extends Component {
+	// react组件中最优先执行的函数
+	constructor(props) {
+		// 继承执行下父级的构造函数
+		super(props)
+		// react定义数据需要定义在状态里面,react状态在this.state中
+		this.state = {
+			inputValue: '',
+			list: []
+		}
+	}
   render() {
     return (
 			<Fragment>
 				<div>
-					<input />
+					<input value={this.state.inputValue} />
 					<button>提交</button>
 				</div>
 				<ul>
