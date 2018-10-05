ad4f41f21885f9fdfd43d766b99ab37006d58507
chao.wang <wcd126yx@126.com>
2018-10-05 09:22:22 +0800

    精简目标你代码

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// PWA, 在支持https服务器上，具备：如果没网，也能看到网页缓存
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
