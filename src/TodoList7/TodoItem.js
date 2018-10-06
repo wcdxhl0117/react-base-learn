cfea17a1e1bd40f6f338ed00af1587bb244c70b6
wangchaoxie <wcd126yx@126.com>
2018-10-06 10:28:00 +0800

    生命周期函数2

--- src/TodoList7/TodoItem.js
+++ src/TodoList7/TodoItem.js
@@ -13,6 +13,22 @@ class TodoItem extends Component {
 			>{content}</div>
 		)
 	}
+
+	/* 
+		componentWillReceiveProps
+		一个组件从父组件接受参数
+		如果这个组件第一次存在于父组件中,不会执行
+		如果之前已经存在于父组件中,才会执行
+	*/
+	componentWillReceiveProps() {
+		console.log('child:componentWillReceiveProps')
+	}
+
+	// componentWillReceiveProps: 组件即将从页面中剔除时,会被执行
+	componentWillUnmount() {
+		console.log('child: componentWillReceiveProps')
+	}
+
 	handleClick() {
 		const { index, deleteItem } = this.props 
 		deleteItem(index)
--- src/TodoList7/TodoList.js
+++ src/TodoList7/TodoList.js
@@ -2,6 +2,24 @@ import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem'
 import './style.css'
 
+/* 
+	生命周期函数是指在某一个时刻组件会自动调用执行的函数
+	注意：constructor符合生命周期定义，但是他不是react独有的，他是ES6自带的，所以不归类在react生命周期函数里
+	1.Mounting(挂在)：componentWillMount(挂载前)，render(渲染)，componentDidMount(挂载完毕)
+	2.Updation:
+		props改变：componentWillReceiveProps(props改变时特有)
+				  shouldComponentUpdate(返回true时往下执行，返回false不执行下面更新的周期函数)
+				  componentWillUpdate(即将更新，即更新前执行)
+				  render(跟新)
+				  componentDidUpdate(跟新结束)
+
+		state改变：shouldComponentUpdate(返回true时往下执行，返回false不执行下面更新的周期函数)
+				  componentWillUpdate(即将更新，即更新前执行)
+				  render(跟新)
+				  componentDidUpdate(跟新结束)
+
+	3.Unmounting: componentWillUnmount(组件卸载前)
+*/
 class TodoList extends Component {
 	constructor(props) {
 		super(props)
@@ -13,8 +31,16 @@ class TodoList extends Component {
 		this.handleBtnClick = this.handleBtnClick.bind(this)
 		this.handleDeleteItem = this.handleDeleteItem.bind(this)
 	}
-  render() {
-    return (
+
+	// componentWillMount：当组件即将被挂载到页面的时刻执行（一次）
+	componentWillMount() {
+		console.log('1.componentWillMount')
+	}
+
+	// render
+	render() {
+		console.log('2.render')
+		return (
 			<Fragment>
 				<div>
 					<label htmlFor='inputLabel'>请输入内容</label>
@@ -38,6 +64,43 @@ class TodoList extends Component {
 			</Fragment>
     	)
 	}
+
+	// componentDidMount：当组建挂载之后执行（一次）
+	componentDidMount() {
+		console.log('3.componentDidMount')
+	}
+
+	// shouldComponentUpdate（组件被更新之前被执行）: 必须返回布尔值，返回true则继续，返回false中断
+	shouldComponentUpdate() {
+		console.log('shouldComponentUpdate')
+		return true
+	}
+
+	/* 
+		componentWillUpdate,组件更新之前执行，但是在shouldComponentUpdate之后执行，
+		如果shouldComponentUpdate返回true执行，如果返回false，就不执行了
+	*/
+	componentWillUpdate() {
+		console.log('componentWillUpdate')
+	}
+
+	// componentDidUpdate: 组件更新完成之后执行
+	componentDidUpdate() {
+		console.log('componentDidUpdate')
+	}
+
+	// componentWillReceiveProps:在TodoItem有props,这个效果在TodoItem
+	componentWillReceiveProps() {
+		console.log('father: componentWillReceiveProps')
+	}
+
+	// componentWillReceiveProps: 在TodoItem看效果
+	componentWillUnmount() {
+		console.log('father: componentWillReceiveProps')
+	}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// 下面都是私有函数
 	getTodoItem() {
 		return this.state.list.map((item, index) => {
 			return (
@@ -53,8 +116,8 @@ class TodoList extends Component {
 	// 输入
 	handleInputChange(e) {
 		// 在react中可以用e.target获取到元素节点，也可以用ref
-		console.log(e.target)
-		console.log(this.input)
+		// console.log(e.target)
+		// console.log(this.input)
 		const value = e.target.value
 		this.setState(() => {
 			return {
@@ -65,6 +128,7 @@ class TodoList extends Component {
 	// 新增
 	handleBtnClick() {
 		// setState第二个参数,setState是异步函数，第二个函数是setState执行完再执行
+		// setState和ref合用会有坑，记得将ref放在第二个参数函数里面
 		this.setState((prevState) => {
 			return {
 				list: [...prevState.list, prevState.inputValue],
