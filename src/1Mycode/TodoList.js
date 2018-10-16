04801c1c0b932e635471ae644241c7dba36a20d9
chao.wang <chao.wang@elanking.com>
2018-10-16 16:18:06 +0800

    新增项目mycode

---
+++ src/1Mycode/TodoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import { Input, Button, List } from 'antd';
+
+class TodoList extends Component {
+	// 在constructor去调用store
+	constructor(props) {
+		super(props);
+	}
+
+  render() {
+    return (
+			<div className="wrap">
+				
+			</div>
+    	)
+	}	
+}
+
+export default TodoList;
